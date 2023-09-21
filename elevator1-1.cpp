a33d44f223384de52f2d8f1fcc8283be9bd0aa5b
keke <2868490398@qq.com>
2023-09-21 13:12:25 +0800

    1-1.cpp

---
+++ elevator1-1.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int floor[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d 0 0\n", C);//打印开始运行时状态
+	int i;
+	i = fabs(A - C);
+	printf("%d %d 1\n", A, i);//打印到达人员所在楼层时状态
+	i = fabs(A - C) + fabs(A - B);
+	printf("%d %d 0\n", B, i);//打印到达目标楼层时状态
+	return 0;
+}
